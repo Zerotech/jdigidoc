1d128e532d1b146f439ad32c8ead9ec6c9230207
Mikk Kirstein <mikk.kirstein@zerotech.ee>
2012-05-03 14:06:22 +0300

    minor refactoring

--- src/test/java/ee/sk/digidoc/CreateDDOCContainerTests.java
+++ src/test/java/ee/sk/digidoc/CreateDDOCContainerTests.java
@@ -1,44 +1,85 @@
 package ee.sk.digidoc;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.InputStream;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CreateDDOCContainerTests {
+    
+    private SignedDoc signedDoc;
+    private DataFile dataFile;
+    private File targetDDOCFile;
+    private File targetBDOCFile;
+    
+    @Test
+    @Before
+    public void setUp() throws Exception {
+        signedDoc = new SignedDoc(SignedDoc.FORMAT_DIGIDOC_XML, SignedDoc.VERSION_1_3);
+        dataFile = new DataFile("D0", DataFile.CONTENT_EMBEDDED_BASE64, "log4j", "text/plain", signedDoc);
+    }
+    
+    @Test
+    @Before
+    public void setTarget() {
+        targetBDOCFile =  new File("target/test.bdoc");
+        targetDDOCFile =  new File("target/test.ddoc");
+        assertFalse(targetDDOCFile.exists());
+        assertFalse(targetBDOCFile.exists());
 
+    }
+
+    @After
+    public void tearDown() {
+        if (targetDDOCFile != null && targetDDOCFile.exists()) {
+            targetDDOCFile.delete();
+        }
+        if (targetBDOCFile != null && targetBDOCFile.exists()) {
+            targetBDOCFile.delete();
+        }
+    }
+    
     @Test
     public void createDDOCFile() throws Exception {
-        SignedDoc sd = new SignedDoc(SignedDoc.FORMAT_DIGIDOC_XML, SignedDoc.VERSION_1_3);
-        sd.addDataFile(new File("src/test/resources/log4j.properties"), "text/plain", DataFile.CONTENT_EMBEDDED_BASE64);
-        sd.writeToFile(new File("target/test.ddoc"));
+        assertFalse(targetDDOCFile.exists());
+        
+        signedDoc.addDataFile(new File("src/test/resources/log4j.properties"), "text/plain", DataFile.CONTENT_EMBEDDED_BASE64);
+        signedDoc.writeToFile(targetDDOCFile);
+        
+        assertTrue(targetDDOCFile.exists());
     }
 
     @Test
     public void createDDOCFileFromStream() throws Exception {
-        SignedDoc sd = new SignedDoc(SignedDoc.FORMAT_DIGIDOC_XML, SignedDoc.VERSION_1_3);
-        DataFile df = new DataFile("D0", DataFile.CONTENT_EMBEDDED_BASE64, "log4j", "text/plain", sd);
-
         InputStream is = this.getClass().getClassLoader().getResourceAsStream("log4j.properties");
+        
         assertNotNull(is);
-        df.setBodyFromStream(is);
-
-        sd.addDataFile(df);
-        sd.writeToFile(new File("target/test.ddoc"));
+        assertFalse(targetDDOCFile.exists());
+        
+        dataFile.setBodyFromStream(is);
+        signedDoc.addDataFile(dataFile);
+        
+        signedDoc.writeToFile(targetDDOCFile);
+        assertTrue(targetDDOCFile.exists());
     }
 
     @Test
     public void createBDOCFileFromStream() throws Exception {
-        SignedDoc sd = new SignedDoc(SignedDoc.FORMAT_BDOC, SignedDoc.BDOC_VERSION_1_0);
-        DataFile df = new DataFile("D0", DataFile.CONTENT_EMBEDDED, "log4j", "text/plain", sd);
-
         InputStream is = this.getClass().getClassLoader().getResourceAsStream("log4j.properties");
+        
         assertNotNull(is);
-        df.setBodyFromStream(is);
-
-        sd.addDataFile(df);
-        sd.writeToFile(new File("target/test.bdoc"));
+        assertFalse(targetBDOCFile.exists());
+        
+        dataFile.setBodyFromStream(is);
+        signedDoc.addDataFile(dataFile);
+        signedDoc.writeToFile(targetBDOCFile);
+        
+        assertTrue(targetBDOCFile.exists());
     }
 }
